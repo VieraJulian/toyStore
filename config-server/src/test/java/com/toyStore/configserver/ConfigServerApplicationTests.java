a8ec824ac23f5616e793cbfdfbad5d3229dc3258
VieraJulian <julianviera2019@hotmail.com>
2024-02-27 19:53:43 -0300

    Julián - Obtener información desde un servidor

---
+++ config-server/src/test/java/com/toyStore/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyStore.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
